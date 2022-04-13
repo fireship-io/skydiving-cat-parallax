563ec6e1234babdce2817ffaa5cfb15746e59a56
Jeff Delaney <hello@fireship.io>
2022-04-13 07:21:44 -0700

    full working demo

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Spring Parallax Demo
+
+A skydiving cat with React Spring Parallax.
---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { useRef } from 'react';
+import moon from './moon.png';
+import land from './land.png';
+import cat from './cat.gif';
+
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+
+function App() {
+  const ref = useRef();
+
+  return (
+    <div>
+      <Parallax pages={4} ref={ref}>
+        {/* <ParallaxLayer speed={1}>
+            <h2>Welcome to my website</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={0.5}>
+            <h2>Web development is fun!</h2>
+        </ParallaxLayer> */}
+
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          factor={2}
+          style={{
+            backgroundImage: `url(${moon})`,
+            backgroundSize: 'cover',
+          }}
+        />
+
+        <ParallaxLayer
+          offset={2}
+          speed={1}
+          factor={4}
+          style={{
+            backgroundImage: `url(${land})`,
+            backgroundSize: 'cover',
+          }}
+        ></ParallaxLayer>
+
+        <ParallaxLayer
+          sticky={{ start: 0.9, end: 2.5 }}
+          style={{ textAlign: 'center' }}
+        >
+          <img src={cat} />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={0.2}
+          speed={0.05}
+          onClick={() => ref.current.scrollTo(3)}
+        >
+          <h2>Welcome to my website</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={3.2}
+          speed={2}
+          onClick={() => ref.current.scrollTo(0)}
+        >
+          <h2>Hi Mom!</h2>
+        </ParallaxLayer>
+      </Parallax>
+    </div>
+  );
+}
+
+export default App;
